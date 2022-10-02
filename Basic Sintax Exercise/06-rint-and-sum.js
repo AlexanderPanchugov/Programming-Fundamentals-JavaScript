b249143256194a711aa2966f4d0b25ea5c95f945
AlexanderPanchugov <114020069+AlexanderPanchugov@users.noreply.github.com>
2022-10-02 22:54:53 +0300

    Add files via upload

---
+++ Basic Sintax Exercise/06-rint-and-sum.js
@@ -0,0 +1,13 @@
+function solve(start, end){
+    let sum = 0;
+    let output = ''
+    for (let i = start; i <= end; i ++){
+        sum += i;
+        output += `${i} `
+        
+    }
+    console.log(output);
+    console.log(`Sum: ${sum}`);
+}
+
+solve(50, 60)
---
+++ Basic Sintax Exercise/08-multiplication-table.js
@@ -0,0 +1,10 @@
+function table(number) {
+
+
+    for (let i = 1; i <= 10; i++) {
+        let result = number * i;
+        console.log(`${number} X ${i} = ${result}`)
+    }
+}
+
+table(5)
